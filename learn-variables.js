33be816238e828f10d1d162c25da29304284b9d7
Thao Le My <mythao189@gmail.com>
2022-08-15 09:06:26 +0700

    first commit

---
+++ learn-variables.js
@@ -0,0 +1,23 @@
+// Declare my age
+const myAge = 37;
+ 
+// Early years of mine
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+ 
+// later years after 2 days
+let laterYears = myAge - 2;
+ 
+//count years of later years
+laterYears = laterYears * 4;
+console.log(earlyYears);
+console.log(laterYears);
+ 
+//count my age in dog years
+const myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+const myName = 'My Thao';
+ 
+//change my name in lowercase mode.
+console.log(myName.toLowerCase());
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
